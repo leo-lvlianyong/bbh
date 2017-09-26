32f3639882a4cde05fdbc524ea413542b37b942e
Leo <leo_lvlianyong@foxmail.com>
2017-09-26 23:26:34 +0800

    增加cocopods

---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, '9.0'
+use_frameworks!
+ 
+target 'bbh' do
+    pod 'Alamofire', '~> 4.0'
+    pod 'SwiftyJSON', '~> 3.0'
+end
--- bbh/Classes/Main/Views/ViewController.swift
+++ bbh/Classes/Main/Views/ViewController.swift
@@ -7,17 +7,43 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        getHTTPJsonData()
+    }
+    
+    func getHTTPJsonData(){
+        let url = URL(string: "http://www.chepintou.cn:3541/car.wap/test/getJsonData")
+        Alamofire.request(url!).validate().responseJSON { response in
+            switch response.result.isSuccess {
+                case true:
+                    if let value = response.result.value {
+                        let json = JSON(value)
+                        if let code = json[0]["code"].string {
+                            print("code:\(code)")
+                        }
+                    }
+                case false:
+                    print(123)
+            }
+        }
+    }
+    
+    func getHTTP(){
+        Alamofire.request("http://www.chepintou.cn:3541/car.wap/test/getJsonData")
+            .responseString { response in
+                print("Success: \(response.result.isSuccess)")
+                print("Response String: \(String(describing: response.result.value))")
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
